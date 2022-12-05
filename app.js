b9b7a70cb2a6ae6797578b53964ecf496bc7b907
Ryuga <vaishnav2353@gmail.com>
2022-12-05 21:55:36 +0530

    Minor update

--- app.js
+++ app.js
@@ -1,50 +1,51 @@
-const path = require('path');
-require('dotenv').config();
+const path = require("path");
+require("dotenv").config();
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
-const csrf = require('csurf');
-const flash = require('connect-flash');
-const morgan = require('morgan');
-const passport = require('passport');
-const multer = require('multer');
-const compression = require('compression')
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
+const flash = require("connect-flash");
+const morgan = require("morgan");
+const passport = require("passport");
+const multer = require("multer");
+const compression = require("compression");
 
+require("./util/passport")(passport);
 
-require('./util/passport')(passport)
-
-const errorController = require('./controllers/error');
-const User = require('./models/user');
-const adminUser = require('./models/admin-user');
+const errorController = require("./controllers/error");
+const User = require("./models/user");
+const adminUser = require("./models/admin-user");
 
 const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.7opvu.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
 
 const app = express();
 const store = new MongoDBStore({
   uri: MONGODB_URI,
-  collection: 'sessions'
+  collection: "sessions",
 });
-
 
 const csrfProtection = csrf();
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'images');
+    cb(null, "images");
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname);
-  }
+    cb(
+      null,
+      new Date().toISOString().replace(/:/g, "-") + "-" + file.originalname
+    );
+  },
 });
 
 const fileFilter = (req, file, cb) => {
   if (
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
   ) {
     cb(null, true);
   } else {
@@ -52,56 +53,55 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+const adminRoutes = require("./routes/admin");
+const userRoutes = require("./routes/user");
+const authRoutes = require("./routes/auth");
 
-const adminRoutes = require('./routes/admin');
-const userRoutes = require('./routes/user');
-const authRoutes = require('./routes/auth');
-
-app.use(compression())
+app.use(compression());
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ storage: fileStorage, fileFilter: fileFilter  }).single('image'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "images")));
 app.use(
   session({
-    secret: 'my secret',
+    secret: "my secret",
     resave: false,
     saveUninitialized: false,
-    store: store
+    store: store,
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 app.use((req, res, next) => {
-  if (req.path === '/callback') {
+  if (req.path === "/callback") {
     // This skips CSRF - route will be unprotected
     return next();
-}
-csrfProtection(req, res, next);
+  }
+  csrfProtection(req, res, next);
 });
 app.use(flash());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.session.isLoggedIn;
   res.locals.isAdminAuthenticated = req.session.isAdminLoggedIn;
   res.locals.spotAccess = req.session.spotAccess;
   res.locals.userImage = req.session.userImage;
-  if(req.session.user)
-  {
+  if (req.session.user) {
     res.locals.name = req.session.user.Name;
   }
-  if (req.path === '/callback'){
+  if (req.path === "/callback") {
     next();
-  }
-  else{
+  } else {
     res.locals.csrfToken = req.csrfToken();
   }
-  next(); 
+  next();
 });
 
 app.use((req, res, next) => {
@@ -126,35 +126,34 @@ app.use((req, res, next) => {
           return next();
         }
         req.user = user;
-        
+
         next();
       })
       .catch((err) => console.log(err));
   }
 });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(userRoutes);
 app.use(authRoutes);
 
-app.get('/500', errorController.get500);
+app.get("/500", errorController.get500);
 
 app.use(errorController.get404);
 
-// app.use((error, req, res, next) => {
-//   res.status(500).render('500', {
-//     pageTitle: 'Error!',
-//     path: '/500',
-//     isAuthenticated: req.session.isLoggedIn
-//   });
-// });
+app.use((error, req, res, next) => {
+  res.status(500).render("500", {
+    pageTitle: "Error!",
+    path: "/500",
+    isAuthenticated: req.session.isLoggedIn,
+  });
+});
 
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => {
+  .then((result) => {
     app.listen(process.env.PORT);
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
-  
