dc63417a7e6db4c00ef1e66e948e3470f8a413d2
Sagar <97119837+sagarraj2710@users.noreply.github.com>
2022-12-14 09:07:27 +0530

    changed some content

--- controllers/user.js
+++ controllers/user.js
@@ -730,7 +730,6 @@ exports.postSpotRegistrationsPage = (req, res, next) => {
           const spot = new Spot({
             user: {
               email: user[0].email,
-              userId: user[0]._id,
             },
             paymentDone: paymentDone,
             events: events,
